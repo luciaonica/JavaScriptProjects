37a3b27f014e82f735acf3d9fc363f1b19160a74
luciaonica <102199637+luciaonica@users.noreply.github.com>
2023-05-23 08:06:04 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment(){
+    count += 1
+    countEl.textContent = count
+}
+
+function save(){
+    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+   
+    countEl.textContent = 0
+    count = 0
+}
+
+
+
